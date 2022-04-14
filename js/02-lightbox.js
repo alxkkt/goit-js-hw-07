ae00a33447be726d8f89e993c7659dc28a4cf398
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-14 15:46:51 +0200

    completed simplelightbox page

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createElements = function (items) {
+  return items
+    .map(
+      (item) =>
+        `<a class="gallery__item" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`
+    )
+    .join("");
+};
+
+const galleryMarkup = createElements(galleryItems);
+gallery.innerHTML = galleryMarkup;
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+lightbox.on("show.simplelightbox");
